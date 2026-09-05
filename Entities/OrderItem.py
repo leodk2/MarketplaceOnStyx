961760291ef920eb21ed3f7a5a5a82351cff2289
Leo Rønsholt Mikkelsen <leo@strandjaegervej.dk>
2026-09-05 23:00:52 +0200

    change entities to be dataclasses instead of pydantic models

--- Entities/OrderItem.py
+++ Entities/OrderItem.py
@@ -1,18 +1,18 @@
+from dataclasses import dataclass
 from datetime import datetime
-from pydantic import BaseModel
 
-class OrderItem(BaseModel):
+
+@dataclass
+class OrderItem:
     orderId: int
-    orderItemId:int
+    orderItemId: int
     productId: int
     productName: str
-    sellerId:int
-    unitPrice:float
-    frieghtValue:float
-    quantity:int
-    totalPrice:float
-    totalAmount:float
-    voucher:float
-    shippingLimitDate:datetime
-    
-    
+    sellerId: int
+    unitPrice: float
+    frieghtValue: float
+    quantity: int
+    totalPrice: float
+    totalAmount: float
+    voucher: float
+    shippingLimitDate: datetime
